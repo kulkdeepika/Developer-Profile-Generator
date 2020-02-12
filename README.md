7455081ce3f74ad0e2d2449675efe08432844b1a
Deepika <kulkdeepika@gmail.com>
2020-02-11 18:12:52 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Profile-Generator
 This command-line application will allow for quick and easy generation of profiles with up-to-date information about members of a team in PDF format.
+
+## Usage and instructions
+It is time consuming to have to navigate to each team member's Github profile to get up-to-date information. This command-line application generates a pdf which provides the most relevant information of a member.
+
+The user is asked to enter the username of the person-of-interest's github profile, and he/she can also choose his/her favorite color from the list provided. A PDF will be generated in the same folder, with the selected color theme, with the name converted.PDF.
+
+If the github-user has provided location, it will display the location, and upon clicking, it opens the location in google maps.
+The link to github account and blog is also made available.
+
+## Environment
+You will need axios, electron-html-to and electron (v 5.0.3) npm packages.
---
+++ index.js
@@ -0,0 +1,103 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require('axios');
+
+const convertFactory = require('electron-html-to');
+ 
+const genHTML = require('./generateHTML.js');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser(){
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is the GitHub username?"
+    },
+    {
+      type: "list",
+      name: "favColor",
+      message: "Pick your favourite color:",
+      choices: [
+        'green',
+        'blue',
+        'pink',
+        'red'
+      ]
+    }
+  ]);
+}
+
+promptUser()
+  .then(function(answers) {
+
+  const url = `https://api.github.com/users/${answers.username}`;
+
+  axios.get(url)
+    .then((response) => {
+
+      response.data.color = answers.favColor;// add the selected color as a property in the response object
+
+    axios.get(response.data.repos_url)
+      .then((res) => {
+        let starCount = 0;
+        for(let i=0; i<res.data.length; i++) // find out the total number of github stars
+        {
+            starCount += res.data[i].stargazers_count;
+        }
+                    
+          response.data.NoOfStars = starCount; // add the total stars as a property in response object
+
+          //check if location is provided
+          if(response.data.location === null){
+            response.data.location = ""
+          }
+          // check if company is provided       
+          if(response.data.company === null){
+            response.data.company = ""
+          }
+          else{
+            response.data.company = `Currently ${response.data.company}`;
+          }
+          //check if bio is provided
+          if(response.data.bio === null){
+            response.data.bio = ""
+          }
+                    
+          //generate the html with the required data
+          const generatedHtml = genHTML.generateHTML(response.data);
+
+          //the code below converts the html to PDF
+          var conversion = convertFactory({
+            converterPath: convertFactory.converters.PDF
+          });
+  
+          conversion({ html: generatedHtml }, function(err, result) {
+            if (err) {
+                return console.error(err);
+            }
+
+           console.log(result.logs);
+            result.stream.pipe(fs.createWriteStream('./converted.PDF'));
+            conversion.kill(); 
+      });
+
+      //this writes to a html file; not necessary for the functionality
+      return writeFileAsync("index.html", generatedHtml);
+    })
+      .catch(function(err) {
+        console.log(err);
+    });
+
+  });
+
+})
+ .then(function() {
+   console.log("Successfully wrote to index.html");
+})
+ .catch(function(err) {
+    console.log(err);
+});
